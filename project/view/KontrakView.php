5352760ff791266381f1564883600f1003e58f2d
mdaffarh <daffarizmawan@upi.edu>
2025-05-11 15:05:05 +0700

    update kontrak view

--- project/view/KontrakView.php
+++ project/view/KontrakView.php
@@ -2,9 +2,14 @@
 
 /******************************************
  Asisten Pemrogaman 13 & 14
-******************************************/
+ ******************************************/
 
-interface KontrakView{
+interface KontrakView
+{
 	public function tampil();
+	public function create();
+	public function edit($id);
+	public function add($data);
+	public function update($data);
+	public function delete($id);
 }
-?>
